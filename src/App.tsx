241d3411f2b4bf1f8eeee3dd10dbd9ef6e7c9975
Henry Le <henryle913@outlook.com>
2025-02-22 23:36:20 -0600

    initial commit with test component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SampleHello from "./sampleHello.tsx";
+
+export default function App() {
+    return (
+        <div>
+            <p>sample</p>
+            <SampleHello />
+        </div>
+    );
+}
---
+++ src/sampleHello.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SampleHello = () => {
+    return (
+        <div>Hello From My Package</div>
+    )
+}
+
+export default SampleHello
